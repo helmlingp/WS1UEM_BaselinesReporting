23977ac52ff7f0200df2a1731cbad68d43a72a0f
helmlingp <66004946+helmlingp@users.noreply.github.com>
2021-01-22 17:30:49 +1030

    Device list updates
    Significant updates to list of devices reported. No longer limited to first 20 devices and policies. Also only showing individual policies compliance for devices with Baseline confirmed installed. Updated to include Smartgroup Assignments and Exclusions.

--- WS1BaselinesReporting.ps1
+++ WS1BaselinesReporting.ps1
@@ -108,7 +108,7 @@ Function setupServerAuth {
     write-host "APIKey: $script:apikey"
     write-host "OG Name: $script:OGName"
   }
-
+  #may be able to check if variable exists before making API call. Will make script quicker
   $OGSearch = Get-OG -Server $script:Server -Cred $script:cred -ApiKey $script:ApiKey -OrgGroup $script:OGName -Debug $Debug
   $script:groupuuid = $OGSearch.OrganizationGroups[0].Uuid;
   if($Debug){ 
@@ -126,18 +126,44 @@ Function getBaselineList {
 }
 
 Function getDevicesinBaseline {
-  param([string]$baselineUUID)
-  $APIEndpoint = "$script:Server/api/mdm/groups/$script:groupuuid/baselines/$baselineUUID/devices";
+  param([string]$baselineUUID,
+  [int]$max_results,
+  [string]$status,
+  [string]$compliance_level
+  )
+
+  if(!$max_results){
+    $max_results = 20
+  }
+  if(!$status){
+    $status = "CONFIRMED_INSTALL,CONFIRMED_REMOVAL,FAILED_REMOVAL,PENDING_REBOOT,PENDING_REMOVAL"
+  }
+  if(!$compliance_level){
+    $compliance_level="Compliant,NonCompliant,Intermediate,NotAvailable"
+  }
+
+  $APIEndpoint = "$script:Server/api/mdm/groups/$script:groupuuid/baselines/$baselineUUID/devices?start_index=0&sort_asc=true&max_results=$max_results&sort_by=id&status=$status&compliance_level=$compliance_level";
   $ApiVersion = "1"
   $WebRequest = Invoke-AWApiCommand -Method Get -Endpoint $APIEndpoint -ApiVersion $ApiVersion -Auth $Script:cred -Apikey $Script:apikey -Debug $Debug
 
-  return $WebRequest.results
+  return $WebRequest
 
 }
 
 Function getDevicePolicies {
-  param([string]$baselineUUID, [string]$deviceUUID)
-  $APIEndpoint = "$script:Server/api/mdm/groups/$script:groupuuid/baselines/$baselineUUID/devices/$deviceUUID/policies";
+  param([string]$baselineUUID, 
+  [string]$deviceUUID,
+  [int]$limit,
+  [string]$compliance_level
+  )
+
+  if(!$limit){
+    $limit = 100
+  }
+  if(!$compliance_level){
+    $compliance_level="NonCompliant,NotAvailable"
+  }
+  $APIEndpoint = "$script:Server/api/mdm/groups/$script:groupuuid/baselines/$baselineUUID/devices/$deviceUUID/policies?offset=0&sort_order=asc&limit=$limit&sort_by=compliance_level&compliance_level=$compliance_level";
   $ApiVersion = "1"
   $WebRequest = Invoke-AWApiCommand -Method Get -Endpoint $APIEndpoint -ApiVersion $ApiVersion -Auth $Script:cred -Apikey $Script:apikey -Debug $Debug
 
@@ -165,170 +191,35 @@ Function getBaselineSummary {
 }
 
 function noncompliantdevices {
-  # Report on Non-Compliant Devices and Settings for a selected Baseline
-  write-host "******************************************************************************" -ForegroundColor Cyan
-  write-host "`n Report on Non-Compliant Devices and Settings for a selected Baseline in a given OG" -ForegroundColor Cyan
-  write-host "`n******************************************************************************" -ForegroundColor Cyan
-  Write-2Report -Path $Script:Path -Message "`nReport on Non-Compliant Devices and Settings for a selected Baseline in a given OG" -Level "Header"
-  Write-Host "`nPlease select a Baseline" -ForegroundColor Yellow
-
-  #Connect details
-  setupServerAuth
-
-  ##Get a list of Baselines
-  $BaselineList = getBaselineList
-
-  $ValidChoices = 0..($BaselineList.Count -1)
-
-  $Choice = ''
-  while ([string]::IsNullOrEmpty($Choice)) {
-    $i = 0
-    foreach ($Baseline in $BaselineList) {
-      Write-Host ('{0}: {1}       {2}' -f $i, $Baseline.name, $Baseline.description)
-      $i += 1
-    }
-
-    $Choice = Read-Host -Prompt 'Please choose one of the above items by number '
-    if ($Choice -in $ValidChoices) {
-        $BaselineName = $BaselineList[$Choice].name
-        $BaselineUUID = $BaselineList[$Choice].baselineUUID
-        $BaselineDescription = $BaselineList[$Choice].description
-        $BaselineTemplate = $BaselineList[$Choice].templateName
-        $BaselineCurrentVersion = $BaselineList[$Choice].version
-        $BaselineParentOG = $BaselineList[$Choice].rootLocationGroupName
-        $BaselineAssignmentCount = $BaselineList[$Choice].assignmentCount
-      } else {
-        [console]::Beep(1000, 300)
-        Write-Warning ('    [ {0} ] is NOT a valid selection.' -f $Choice)
-        Write-Warning '    Please try again ...'
-        pause
-
-        $Choice = ''
-      }
-  }
+  #Variables
+  $status = "CONFIRMED_INSTALL,CONFIRMED_REMOVAL,FAILED_REMOVAL,PENDING_REBOOT,PENDING_REMOVAL"
+  $compliance_level = "NonCompliant,Intermediate,NotAvailable"
   
-  ##Get Baseline Summary
-  Write-2Report -Path $Script:Path -Message "`nSummary Information for Baseline" -Level "Header"
-  Write-2Report -Path $Script:Path -Message "Baseline: $BaselineName" -Level "Body"
-  Write-2Report -Path $Script:Path -Message "Description: $BaselineDescription" -Level "Body"
-  Write-2Report -Path $Script:Path -Message "Template: $BaselineTemplate" -Level "Body"
-  Write-2Report -Path $Script:Path -Message "Current Version: $BaselineCurrentVersion" -Level "Body"
-  Write-2Report -Path $Script:Path -Message "Parent OG: $BaselineParentOG" -Level "Body"
-  Write-2Report -Path $Script:Path -Message "Assignment Count: $BaselineAssignmentCount" -Level "Body"
-  
-  $BaselineSummary = getBaselineSummary -baselineUUID $BaselineUUID
+  #Call Report Function
+  report -status $status -compliance_level $compliance_level
 
-  $installsummaryproperties = @(
-    @{N="Status";E={$_.status}},
-    @{N="Count";E={$_.count}},
-    @{N="Reasons";E={$_.reasons}}
-  )
-  $strBaselineSummaryInstalls = $BaselineSummary | Select-Object -ExpandProperty summary | Select-Object -ExpandProperty installs | Select-Object -Property $installsummaryproperties | Format-Table -AutoSize | Out-String
-  Write-2Report -Path $Script:Path -Message "`nInstall Summary" -Level "Header"
-  Write-2Report -Path $Script:Path -Message $strBaselineSummaryInstalls -Level "Body"
-
-  $versionsummaryproperties = @(
-    @{N="Count";E={$_.count}},
-    @{N="Versions";E={$_.version}}
-  )
-  $strBaselineSummaryVersions = $BaselineSummary | Select-Object -ExpandProperty summary | Select-Object -ExpandProperty versions | Select-Object -Property $versionsummaryproperties | Format-Table -AutoSize | Out-String
-  Write-2Report -Path $Script:Path -Message "Version Summary" -Level "Header"
-  Write-2Report -Path $Script:Path -Message $strBaselineSummaryVersions -Level "Body"
-
-  $compliancesummaryproperties = @(
-    @{N="Status";E={$_.status}},
-    @{N="Count";E={$_.count}}
-  )
-  $strBaselineSummaryCompliance = $BaselineSummary | Select-Object -ExpandProperty summary | Select-Object -ExpandProperty compliance | Select-Object -Property $compliancesummaryproperties | Format-Table -AutoSize | Out-String
-  Write-2Report -Path $Script:Path -Message "Compliance Summary" -Level "Header"
-  Write-2Report -Path $Script:Path -Message $strBaselineSummaryCompliance -Level "Body"
-
- $customizationssummaryproperties = @(
-    @{N="Name";E={$_.name}},
-    @{N="Path";E={$_.path}},
-    @{N="Status";E={$_.status}}
-  )
-  $strBaselineSummaryCustomizations = $BaselineSummary | Select-Object -ExpandProperty customizations | Select-Object -Property $customizationssummaryproperties | Sort-Object -Property "Name" | Format-Table -AutoSize | Out-String
-  Write-2Report -Path $Script:Path -Message "Baseline Customizations" -Level "Header"
-  Write-2Report -Path $Script:Path -Message $strBaselineSummaryCustomizations -Level "Body"
-
-  $policysummaryproperties = @(
-    @{N="Name";E={$_.name}},
-    @{N="Path";E={$_.path}},
-    @{N="Status";E={$_.status}}
-  )
-  $strBaselineSummaryPolicies = $BaselineSummary | Select-Object -ExpandProperty policies | Select-Object -Property $policysummaryproperties | Sort-Object -Property "Name" | Format-Table -AutoSize | Out-String
-  Write-2Report -Path $Script:Path -Message "Baseline Additional Policies" -Level "Header"
-  Write-2Report -Path $Script:Path -Message $strBaselineSummaryPolicies -Level "Body"
-
-  ##Get Baseline Assignments
-  $BaselineAssignment = getBaselineAssignments -baselineUUID $BaselineUUID
-  $strBaselineAssignments = $BaselineAssignment | Select-Object -ExpandProperty assigned_smart_groups | Select-Object -Property @(@{N="SmartGroup";E={$_.name}}) | Sort-Object "SmartGroup" | Format-Table -AutoSize | Out-String
-  Write-2Report -Path $Script:Path -Message "Baseline Selected is assigned to the following SmartGroups" -Level "Header"
-  Write-2Report -Path $Script:Path -Message $strBaselineAssignments -Level "Body"
-  $strBaselineExclusions = $BaselineAssignment | Select-Object -ExpandProperty excluded_smart_groups | Select-Object -Property @(@{N="SmartGroup";E={$_.name}}) | Sort-Object "SmartGroup" | Format-Table -AutoSize | Out-String
-  Write-2Report -Path $Script:Path -Message "Baseline Selected is excluded from the following SmartGroups" -Level "Header"
-  Write-2Report -Path $Script:Path -Message $strBaselineExclusions -Level "Body"
-
-  ##Get devices in Baseline
-  $DevicesinBaseline = getDevicesinBaseline -baselineUUID $BaselineUUID
-  $selectnoncompliantDevicesinBaseline = $DevicesinBaseline | Where-Object {$_.compliance.status -eq "Non-Compliant" -or $_.compliance.status -eq "Intermediate" -or $_.compliance.status -eq "NotAvailable"}
-  Write-2Report -Path $Script:Path -Message "Non-Compliant, Intermediate & NotAvailable devices in $BaselineName Baseline" -Level "Header"
-  
-  $deviceproperties = @(
-    @{N="Device Name";E={$_.friendlyName}},
-    @{N="userName";E={$_.userName}},
-    @{N="Install Status";E={$_.status | Select-Object -ExpandProperty status}},
-    @{N="Baseline Version";E={$_.status | Select-Object -ExpandProperty version}},
-    @{N="Compliance Status";E={$_.compliance | Select-Object -ExpandProperty status}}
-  )
-  #$strDevicesinBaseline = $DevicesinBaseline | Where-Object {$_.compliance.status -eq "Non-Compliant" -or $_.compliance.status -eq "Intermediate" -or $_.compliance.status -eq "NotAvailable"} | Select-Object -Property $deviceproperties | Sort-Object "Device Name" | Format-Table -AutoSize | Out-String
-  $strDevicesinBaseline = $selectnoncompliantDevicesinBaseline  | Select-Object -Property $deviceproperties | Sort-Object "Device Name" | Format-Table -AutoSize | Out-String
-  Write-2Report -Path $Script:Path -Message $strDevicesinBaseline -Level "Body"
-
-  #Create array to store Device UUID and Name
-  $devicepoliciesarray = @()
-  foreach ($device in $selectnoncompliantDevicesinBaseline){
-    $DeviceUUID = $device.deviceUUID
-    $DeviceName = $device.friendlyName
-    $DevicePolicies = getDevicePolicies -baselineUUID $BaselineUUID -deviceUUID $DeviceUUID
-    foreach ($policy in $DevicePolicies){
-
-      $PSObject = New-Object PSObject -Property @{
-        DeviceUUID = $DeviceUUID
-        DeviceName = $DeviceName
-        Policy=$policy.name
-        PolicyPath=$policy.path
-        ComplianceStatus=$policy.compliance.status
-      }
-      $devicepoliciesarray += $PSObject
-    }
-  }
-
-  $deviceproperties = @(
-    @{N="Device UUID";E={$_.DeviceUUID}},
-    @{N="Device Name";E={$_.DeviceName}},
-    @{N="Policy";E={$_.Policy}},
-    @{N="PolicyPath";E={$_.PolicyPath}},
-    @{N="Compliance Status";E={$_.ComplianceStatus}}
-  )
-  $strdevicepoliciesarray = $devicepoliciesarray | Select-Object -Property $deviceproperties | Sort-Object -Property @{Expression = {"Device UUID"}; Ascending = $false} | Format-Table -AutoSize | Out-String
-  Write-2Report -Path $Script:Path -Message $strdevicepoliciesarray -Level "Body"
-
-  #Export this list to CSV?
-  $csvLocation = $pathfile+"_"+$BaselineName+".csv"
-  $devicepoliciesarray | Select-Object -Property $deviceproperties | Sort-Object -Property @{Expression = {"Device UUID"}; Ascending = $false} | Export-CSV $csvLocation -noTypeInformation
-
-  Write-2Report -Path $Script:Path -Message "Completed report on Non-Compliant Devices and Settings for $BaselineName Baseline in $BaselineParentOG" -Level "Footer"
-  $devicepoliciesarray = @()
 }
 
 function alldevices {
-  # Report on Non-Compliant Devices and Settings for a selected Baseline
+  #Variables
+  $status = "CONFIRMED_INSTALL,CONFIRMED_REMOVAL,FAILED_REMOVAL,PENDING_REBOOT,PENDING_REMOVAL"
+  $compliance_level = "Compliant,NonCompliant,Intermediate,NotAvailable"
+  
+  #Call Report Function
+  report -status $status -compliance_level $compliance_level
+
+}
+
+function report {
+  param([string]$status,
+  [string]$compliance_level
+  )
+
+  # Report on Devices and Settings for a selected Baseline
   write-host "******************************************************************************" -ForegroundColor Cyan
-  write-host "`n Reporting on All Devices for a selected Baseline in given OG" -ForegroundColor Cyan
+  write-host "`n Report on $compliance_level and Settings for a selected Baseline in a given OG" -ForegroundColor Cyan
   write-host "`n******************************************************************************" -ForegroundColor Cyan
-  Write-2Report -Path $Script:Path -Message "`nReporting on All Devices for a selected Baseline in given OG" -Level "Header"
+  Write-2Report -Path $Script:Path -Message "`nReport on $compliance_level and Settings for a selected Baseline in a given OG" -Level "Header"
   Write-Host "`nPlease select a Baseline" -ForegroundColor Yellow
 
   #Connect details
@@ -402,7 +293,7 @@ function alldevices {
   Write-2Report -Path $Script:Path -Message "Compliance Summary" -Level "Header"
   Write-2Report -Path $Script:Path -Message $strBaselineSummaryCompliance -Level "Body"
 
- $customizationssummaryproperties = @(
+  $customizationssummaryproperties = @(
     @{N="Name";E={$_.name}},
     @{N="Path";E={$_.path}},
     @{N="Status";E={$_.status}}
@@ -429,10 +320,13 @@ function alldevices {
   Write-2Report -Path $Script:Path -Message "Baseline Selected is excluded from the following SmartGroups" -Level "Header"
   Write-2Report -Path $Script:Path -Message $strBaselineExclusions -Level "Body"
 
-  ##Get devices in Baseline
-  $DevicesinBaseline = getDevicesinBaseline -baselineUUID $BaselineUUID
-  $selectnoncompliantDevicesinBaseline = $DevicesinBaseline #| Where-Object {$_.compliance.status -eq "Non-Compliant" -or $_.compliance.status -eq "Intermediate" -or $_.compliance.status -eq "NotAvailable"}
-  Write-2Report -Path $Script:Path -Message "All devices in $BaselineName Baseline" -Level "Header"
+  ##Report on devices in Baseline
+  Write-2Report -Path $Script:Path -Message "$compliance_level devices in $BaselineName Baseline" -Level "Header"
+  $TotalDevicesinBaseline = getDevicesinBaseline -baselineUUID $BaselineUUID
+  $max_results = $TotalDevicesinBaseline.total
+  $selectDevicesinBaseline = getDevicesinBaseline -baselineUUID $BaselineUUID -max_results $max_results -status $status -compliance_level $compliance_level
+  $selectedDevicesinBaseline = $selectDevicesinBaseline.results
+  
   
   $deviceproperties = @(
     @{N="Device Name";E={$_.friendlyName}},
@@ -441,16 +335,26 @@ function alldevices {
     @{N="Baseline Version";E={$_.status | Select-Object -ExpandProperty version}},
     @{N="Compliance Status";E={$_.compliance | Select-Object -ExpandProperty status}}
   )
-  #$strDevicesinBaseline = $DevicesinBaseline | Where-Object {$_.compliance.status -eq "Non-Compliant" -or $_.compliance.status -eq "Intermediate" -or $_.compliance.status -eq "NotAvailable"} | Select-Object -Property $deviceproperties | Sort-Object "Device Name" | Format-Table -AutoSize | Out-String
-  $strDevicesinBaseline = $selectnoncompliantDevicesinBaseline  | Select-Object -Property $deviceproperties | Sort-Object "Device Name" | Format-Table -AutoSize | Out-String
+  $strDevicesinBaseline = $selectedDevicesinBaseline  | Select-Object -Property $deviceproperties | Sort-Object "Device Name" | Format-Table -AutoSize | Out-String
   Write-2Report -Path $Script:Path -Message $strDevicesinBaseline -Level "Body"
+
+  #Report on devices that have the baseline installed, but are non-compliant or partially compliant (Intermediate) and report on individual setting compliance
+  $status = "CONFIRMED_INSTALL"
+  $compliance_level = "NonCompliant,Intermediate"
+  Write-2Report -Path $Script:Path -Message "Settings with $compliance_level for devices with $BaselineName Baseline Installed" -Level "Header"
+  $selectDevicesinBaseline = getDevicesinBaseline -baselineUUID $BaselineUUID -max_results $max_results -status $status -compliance_level $compliance_level
+  $selectedDevicesinBaseline = $selectDevicesinBaseline.results
+  #Could reuse existing and filter, but a lot of processing on the endpoint. Might be better than on the API server. Need to filter status for CONFIRMED_INSTALL also
+  #$selectedDevicesinBaseline = $selectDevicesinBaseline | Where-Object {$_.compliance.status -eq "Non-Compliant" -or $_.compliance.status -eq "Intermediate"}
 
   #Create array to store Device UUID and Name
   $devicepoliciesarray = @()
-  foreach ($device in $selectnoncompliantDevicesinBaseline){
+  foreach ($device in $selectedDevicesinBaseline){
     $DeviceUUID = $device.deviceUUID
     $DeviceName = $device.friendlyName
-    $DevicePolicies = getDevicePolicies -baselineUUID $BaselineUUID -deviceUUID $DeviceUUID
+
+    $compliance_level = "NonCompliant,NotAvailable"
+    $DevicePolicies = getDevicePolicies -baselineUUID $BaselineUUID -deviceUUID $DeviceUUID -limit 100 -compliance_level $compliance_level
     foreach ($policy in $DevicePolicies){
 
       $PSObject = New-Object PSObject -Property @{
@@ -478,7 +382,7 @@ function alldevices {
   $csvLocation = $pathfile+"_"+$BaselineName+".csv"
   $devicepoliciesarray | Select-Object -Property $deviceproperties | Sort-Object -Property @{Expression = {"Device UUID"}; Ascending = $false} | Export-CSV $csvLocation -noTypeInformation
 
-  Write-2Report -Path $Script:Path -Message "Completed report on All Devices and Settings for $BaselineName Baseline in $BaselineParentOG" -Level "Footer"
+  Write-2Report -Path $Script:Path -Message "Completed report on $compliance_level Devices and Settings for $BaselineName Baseline in $BaselineParentOG" -Level "Footer"
   $devicepoliciesarray = @()
 }
 
